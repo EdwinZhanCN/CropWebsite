c6d02b2baaeaa98e0bc7a278ed57e9734aeb5793
EdwinZhan <ed_zhan@outlook.com>
2024-09-29 22:39:33 -0400

    [BugFix] Fix the React Router

--- cropwebsite-frontend/vite.config.js
+++ cropwebsite-frontend/vite.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require('vite')
-const reactRefresh = require('@vitejs/plugin-react-refresh')
+import reactRefresh from '@vitejs/plugin-react-refresh'
 import { fileURLToPath, URL } from 'node:url'
+import {defineConfig} from "vite";
 
 module.exports = defineConfig({
     base: '/',
