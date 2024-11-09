667724d84a4c3f274d801ee10df4e7c95ea38c14
EdwinZhan <ed_zhan@outlook.com>
2024-11-09 00:33:14 -0500

    [BugFix] Env Variable Fix

--- cropwebsite-backend/src/test/java/com/edwinzhan/cropwebsitebackend/CropwebsiteBackendApplicationTests.java
+++ cropwebsite-backend/src/test/java/com/edwinzhan/cropwebsitebackend/CropwebsiteBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.edwinzhan.cropwebsitebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CropwebsiteBackendApplicationTests {
 
     @Test
